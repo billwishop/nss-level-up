3fcf946c928e29998cd0c0f69f672e65d5583c3b
billwishop <willbishop.hello@gmail.com>
2021-02-05 14:07:25 -0600

    Completed Ch4

--- Level Up.session.sql
+++ Level Up.session.sql
@@ -1,1 +1,6 @@
 SELECT * FROM levelupapi_gametypes;
+SELECT * FROM auth_user;
+SELECT * FROM authtoken_token;
+SELECT * FROM levelupapi_gamer;
+SELECT * FROM levelupapi_games;
+SELECT * FROM levelupapi_event;
